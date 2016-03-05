46fb480f6cc2347d1c92dfaf8b6194a193f01f37
Bryan Nissen <bryan@webizly.com>
2016-03-05 18:14:32 -0500

    feat(feature) Started Writting Features Service.  Create Route add and tested.

--- client/app/feature/feature.service.js
+++ client/app/feature/feature.service.js
@@ -16,9 +16,44 @@
         return FeatureRestangular;
       }
     ])
-    .factory('Features', ['FeatureRestangular', function () {
-      //var featureApi;
+    .factory('Features', ['FeatureRestangular', '$q', function (FeatureRestangular, $q) {
+      var featureApi = FeatureRestangular.one('features');
+
+      /**
+       * Helper Function that returns a rejected promise with a variable message
+       * @param {string} message - Message to return
+       * @returns {Object} -> {Rejection Message}
+       */
+      function returnError(message) {
+        var deferred = $q.defer();
+        deferred.reject(message);
+        return deferred.promise;
+      }
+
       return {
+
+        create: function (feature) {
+          if (!angular.isObject(feature)) {
+            return returnError('Provided Feature must be an Object');
+          }
+          return featureApi.post(null, feature);
+        },
+
+        get: function (_id) {
+          return _id;
+        },
+
+        update: function (feature) {
+          return feature;
+        },
+
+        delete: function (feature) {
+          return feature;
+        },
+
+        getList: function () {
+
+        }
 
       };
     }]);
--- client/app/feature/feature.service.spec.js
+++ client/app/feature/feature.service.spec.js
@@ -1,4 +1,5 @@
 /*jshint expr: true*/
+/*globals afterEach */
 (function () {
   'use strict';
 
@@ -66,19 +67,85 @@
 
     describe('Service: Features', function () {
 
-      var Features;
+      var Features,
+        exampleFeature,
+        $rootScope;
 
       // Initialize the Service
-      beforeEach(inject(function (_Features_) {
+      beforeEach(inject(function (_Features_, _$rootScope_) {
         Features = _Features_;
+        exampleFeature = angular.copy(feature1);
+        $rootScope = _$rootScope_;
       }));
 
       it ('should exist', function () {
         expect(Features).to.exist;
       });
 
-      it ('should send a POST request to CREATE a Feature Object', function () {
-        
+      describe('CREATE Feature Object', function () {
+
+        // Happy Path
+        it ('should send a POST request to CREATE a Feature Object', function () {
+          var resFeature;
+
+          // Expected API Request
+          $httpBackend.expectPOST('/api/features', {
+            name: 'Feature',
+            description: 'Feature Description',
+            expectedUses: 0,
+            modifiers: [{
+              field: 'armorClass',
+              value: 4,
+              operator: 'plus'
+            }, {
+              field: 'damagePerRound',
+              value: 3,
+              operator: 'minus',
+              modifiesEffective: false
+            }]
+          }).respond(200, feature1);
+
+          // Function Call and Response Listeners
+          Features.create({
+            name: 'Feature',
+            description: 'Feature Description',
+            expectedUses: 0,
+            modifiers: [{
+              field: 'armorClass',
+              value: 4,
+              operator: 'plus'
+            }, {
+              field: 'damagePerRound',
+              value: 3,
+              operator: 'minus',
+              modifiesEffective: false
+            }]
+          }).then(function (result) {  // Promise Callback (Should be Called)
+            resFeature = result.plain();
+          }).catch(function () {      // Error Callback (Should not be called)
+            expect(false).to.be.true;
+          });
+
+          // Verify returned response
+          $httpBackend.flush();
+          expect(resFeature).to.eql(feature1);
+        });
+
+        // Error Path 1
+        it ('should return an error if the provided Feature is not an object', function () {
+          var returnedError;
+
+          // Make improper Function call
+          Features.create().then(function () {
+            expect(false).to.be.true;
+          }).catch(function (errorMessage) {
+            returnedError = errorMessage;
+          });
+
+          // Digest Promise and Check Error
+          $rootScope.$digest();
+          expect(returnedError).to.equal('Provided Feature must be an Object');
+        });
       });
 
       it ('should send a GET request to READ a Feature Object', function () {
